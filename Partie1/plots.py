ed11e8f223a2a05819cfd0604df45c13aa069ec1
StefanCastana <stefanrotaru07@gmail.com>
2022-12-05 20:00:16 +0100

    j'ai commit

--- Partie1/plots.py
+++ Partie1/plots.py
@@ -8,41 +8,25 @@ data1=pd.read_csv("perf_philo.csv", sep=';')
 data2=pd.read_csv("perf_wr_read.csv", sep=';')
 data3=pd.read_csv("perf_prod_cons.csv", sep=';')
 
-def graph(d, title, xlab, ylab, mean):
+def graph(d, title, xlab, ylab):
     plt.title(title)
-    plt.boxplot(d)
+    plt.boxplot(d, showmeans=True)
     plt.xlabel(xlab)
     plt.ylabel(ylab)
-    y = np.arange(1,8,1)
     plt.xticks([1,2,3,4,5,6,7],t)
-    plt.scatter(y, mean, label='Mean')
-    plt.plot()
     plt.show()
 
 arr1=np.zeros((n,5))
 arr2=np.zeros((n,5))
 arr3=np.zeros((n,5))
-mean1 = np.zeros(n)
-mean2 = np.zeros(n)
-mean3 = np.zeros(n)
 
-j = 0
-i = 0
-while(i < n):
-    while (j < 5):
+for i in range(n):
+    for j in range(5):
         arr1[i][j] = data1[" Time elapsed [s]"][i*5 + j]
         arr2[i][j] = data2[" Time elapsed [s]"][i*5 + j]
         arr3[i][j] = data3[" Time elapsed [s]"][i*5 + j]
-        mean1[i] += arr1[i][j] 
-        mean2[i] += arr2[i][j] 
-        mean3[i] += arr3[i][j] 
-        j += 1
-    mean1[i] /= 5
-    mean2[i] /= 5
-    mean3[i] /= 5
-    i += 1
-    j = 0
 
-graph(arr1.transpose(),"Performance of Philosophers", "Number of threads", "Time elapsed [s]", mean1)
-graph(arr2.transpose(),"Performance of Producers Consumers", "Number of threads", "Time elapsed [s]", mean2)
-graph(arr3.transpose(),"Performance of writer reader", "Number of threads", "Time elapsed [s]", mean3)
+
+graph(arr1.transpose(),"Performance of Philosophers", "Number of threads", "Time elapsed [s]")
+graph(arr2.transpose(),"Performance of writer reader", "Number of threads", "Time elapsed [s]")
+graph(arr3.transpose(),"Performance of Producers Consumers", "Number of threads", "Time elapsed [s]")
---
+++ Partie2/assembleur.c
@@ -0,0 +1,56 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int lockThread = 0;
+
+void lock(){
+    asm(
+        "enter:"
+        "movl $1, %%eax;"
+        "xchgl %%eax, %0;"
+        "testl %%eax, %%eax;"
+        "jnz enter; "
+        :"=r" (lockThread)
+        :
+        :"eax"
+        );
+    printf("%d",lockThread);
+};
+
+void unlock(){
+    asm(
+        "movl $0, %0 ;"
+        :"=r" (lockThread)
+        :
+        :"eax"
+        );
+    printf("%d",lockThread);
+    }
+
+void sectionCritique(int N){
+    for (int i = 0; i < 6400/N; i++)
+    {
+        while(lockThread == 1){}
+    lock();
+    for (int i = 0; i < 10000; i++){}
+    unlock();
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int N = atoi(argv[1]);
+    pthread_t Threads[N];
+    for (int i = 0; i < N; i++)
+    {
+        if(pthread_create(&Threads(i), NULL, (void*) sectionCritique, NULL) != 0){
+            printf("error");
+        }
+    }
+    
+    return 0;
+}
