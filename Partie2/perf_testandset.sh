dc94c21c49bccb0262b4fa96aee198b3c46f01fb
StefanCastana <stefanrotaru07@gmail.com>
2022-12-05 22:39:46 +0100

    YAS JE JOUI PTN T BONNE

--- Partie2/perf_testandset.sh
+++ Partie2/perf_testandset.sh
@@ -3,12 +3,16 @@
 
 touch f.txt
 touch p.csv
+echo "Number of threads; Measure; Time elapsed [s]" >> p.csv
 gcc verrou.c -o verrou.out
 for i in $(seq 1 1 $1)
 do
-    A=$((6400/$i))
-    $(/usr/bin/time -f %e -o f.txt ./verrou.out $A)
-    echo "$i; $(cat f.txt) " &>> p.csv
+    for j in 1 2 3 4 5
+        do
+        A=$((6400/$i))
+        $(/usr/bin/time -f %e -o f.txt ./verrou.out $A)
+        echo "$i;$j;$(cat f.txt) " &>> p.csv
+        done
 done
 rm *.out
 rm f.txt
---
+++ Partie2/plot2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt 
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("p.csv", sep=";")
+
+def graph(d, title, xlab, ylab):
+    plt.title(title)
+    plt.boxplot(d, showmeans=True)
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    plt.show()
+
+n=len(data[" Time elapsed [s]"]) // 5
+arr = np.zeros((n,5))
+for i in range(n):
+    for j in range(5):
+        arr[i][j] = data[" Time elapsed [s]"][i*5 + j]
+
+print(arr)
+graph(arr.transpose(), "ok", "x", 'Y')
